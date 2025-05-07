a6beafd7b1c039df75efd673e1378fdacc8c87c8
FonzAye <george_super@ukr.net>
2025-05-07 18:59:36 +0300

    fixed file structure

--- modules/network/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "subnet_ids" {
-  description = "Map of subnet keys to their IDs and attributes"
-  value = { for k, subnet in aws_subnet.subnets : k => {
-    # name = subnet.tags["Name"]  # Assuming 'Name' is set in tags
-    id   = subnet.id
-    zone = subnet.availability_zone
-    }
-  }
-}
