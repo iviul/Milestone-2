f09821a555570eb565b3aafbbd4138fcf4fd0ee9
illusion4 <vladyslavlintur@gmail.com>
2025-05-08 16:12:03 +0300

    delete redunant module

--- terraform/modules/vms/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "public_ips" {
-  value = { for k, vm in aws_instance.vm : k => vm.public_ip }
-}
-
-output "private_ips" {
-  value = { for k, vm in aws_instance.vm : k => vm.private_ip }
-}
--- terraform/modules/vms/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "vm_config" { }
-
-variable "vpc_id" {
-    type = string
-}
-
-variable "subnet_ids_by_name" {
-    type = map(string)
-}
