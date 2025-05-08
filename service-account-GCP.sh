007998513177a2ec9ae8f78a6ef51188c5edc4d6
DolVladzio <dolynkavladzio@gmail.com>
2025-05-08 09:28:30 +0000

    Updated code

--- service-account-GCP.sh
+++ service-account-GCP.sh
@@ -6,6 +6,7 @@ ROLE=$2
 PROJECT_ID=$(gcloud config get-value project)
 DESCRIPTION="The service account for the Terraform"
 KEY_FILE=$3
+BUCKET_NAME=$4
 #######################################################
 if [[ -z "$PROJECT_ID" ]]; then
     echo "- Error: Unable to retrieve GCP project ID. Ensure your GCP project is set."
@@ -22,7 +23,6 @@ REQUIRED_APIS=(
     "serviceusage.googleapis.com"
     "storage.googleapis.com"
 )
-
 echo "=== Enabling required APIs for project: $PROJECT_ID ==="
 for api in "${REQUIRED_APIS[@]}"; do
     echo "- Enabling '$api'..."
@@ -52,14 +52,14 @@ else
 fi
 #######################################################
 echo "=== Binding role '$ROLE' to the service account... ==="
-if ! gcloud projects add-iam-policy-binding "$PROJECT_ID" \
+if gcloud projects add-iam-policy-binding "$PROJECT_ID" \
     --member="serviceAccount:$SERVICE_ACCOUNT_NAME@$PROJECT_ID.iam.gserviceaccount.com" \
     --role="$ROLE"; then
+    echo "=== The role '$ROLE' was bound to the service account! ==="
+    echo
+else
     echo "=== Error: Failed to bind role '$ROLE' to the service account. ==="
     exit 1
-else
-    echo "=== Role 'editor' was bound ==="
-    echo
 fi
 #######################################################
 echo "=== Generating key file for $SERVICE_ACCOUNT_NAME... ==="
@@ -75,12 +75,33 @@ fi
 echo "=== Activating service account using the key... ==="
 if gcloud auth activate-service-account \
     --key-file=$KEY_FILE; then
-    echo "=== Service account activated successfully. ==="
+    echo "=== Service account activated successfully! ==="
     echo
 else
     echo "=== Error: Failed to activate the service account. ==="
     exit 1
 fi
 #######################################################
-echo "=== The service account - '$SERVICE_ACCOUNT_NAME' successfully configured via GCP CLI profile. ==="
+echo "=== The service account - '$SERVICE_ACCOUNT_NAME' successfully configured via GCP CLI profile! ==="
+echo
+#######################################################
+echo "=== Creating the bucket 'gs://$BUCKET_NAME'...  ==="
+if gcloud storage buckets create gs://$BUCKET_NAME \
+    --location=us-central1 \
+    --uniform-bucket-level-access; then
+    echo "=== The bucket 'gs://$BUCKET_NAME' was created successfully! ==="
+    echo
+else
+    echo "=== Error: a bucket 'gs://$BUCKET_NAME' wasn't created. The bucket name '$BUCKET_NAME' already exists, try another name ==="
+    exit 1
+fi
+#######################################################
+echo "=== Enabling versioning for 'gs://$BUCKET_NAME'... ==="
+if gsutil versioning set on gs://$BUCKET_NAME; then
+    echo "=== Enabled versioning for 'gs://$BUCKET_NAME'! ==="
+    echo
+else
+    echo "=== Wasn't enabled versioning for 'gs://$BUCKET_NAME' ==="
+    exit 1
+fi
 #######################################################
