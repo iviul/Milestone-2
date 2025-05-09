4e7546d3d460853858120eef01f808660a444266
Dima Artamonov <artamonovdima355@gmail.com>
2025-05-09 14:23:41 +0300

    Configure TF to use remote state and service account

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -9,8 +9,7 @@ terraform {
 
 provider "aws" {
   region     = var.region
-  access_key = var.aws_access_key_id
-  secret_key = var.aws_secret_access_key
+  profile = var.aws_user
 }
 
 module "network" {
