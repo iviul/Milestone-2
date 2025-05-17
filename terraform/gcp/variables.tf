0c985504f1f669684b073e4a2dcf9cf8953ab7fc
Bohdan Rublevskyi <rublevkyibohdan@gmail.com>
2025-05-17 12:36:26 +0000

    remove unused vars

--- terraform/gcp/variables.tf
+++ terraform/gcp/variables.tf
@@ -1,14 +1,3 @@
-variable "project_id" {
-  description = "The GCP project ID"
-  type        = string
-}
-
-variable "region" {
-  description = "Region for the network"
-  type        = string
-  default     = "europe-central2"
-}
-
 variable "private_key_path" {
   type        = string
   description = "Path to a private key"
