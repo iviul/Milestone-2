181cb7f4934dbd45baf39bfd0c1c46a1cbf1c6c4
Bohdan Rublevskyi <rublevkyibohdan@gmail.com>
2025-05-29 17:35:28 +0000

    refactor names of ansible roles and terraform modules, fix majority of lint issues for ansible roles

--- terraform/gcp/inventory.tf
+++ terraform/gcp/inventory.tf
@@ -26,6 +26,11 @@ locals {
     public_ips      = module.vm.public_ips
     # bastion_ip = module.vm.private_ips["bastion"]
 
+    db_host     = module.db-instance.db_hosts[local.db_name]
+    db_user     = module.db-instance.db_users[local.db_name]
+    db_password = module.db-instance.db_passwords[local.db_name]
+    db_port     = module.db-instance.db_ports[local.db_name]
+    db_name     = module.db-instance.db_names[local.db_name]
 
     db_host     = module.db-instance.db_hosts[local.db_name]
     db_user     = module.db-instance.db_users[local.db_name]
