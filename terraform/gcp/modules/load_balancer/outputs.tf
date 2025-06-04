9beb765322c0a5fc3c52947a7077c8e229afd261
Oleh Kuzo <olegkuzo23@gmail.com>
2025-06-04 10:12:06 +0300

    stash

--- terraform/gcp/modules/load_balancer/outputs.tf
+++ terraform/gcp/modules/load_balancer/outputs.tf
@@ -1,8 +1,4 @@
 output "forwarding_rule_ip" {
-  description = "The IP address of the regional TCP load balancer"
-  value       = google_compute_forwarding_rule.k3s_forwarding_rule.ip_address
+  description = "The forwarding rule IP address."
+  value       = google_compute_forwarding_rule.lb.ip_address
 }
-
-# output "ip_address" {
-#   value = google_compute_global_address.lb_address.address  // adjust this reference based on your resource
-# }
