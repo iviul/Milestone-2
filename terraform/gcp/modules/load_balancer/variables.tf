2ae994ea86e0c5652c15c03cf5dec96e3f584f0d
Oleh Kuzo <olegkuzo23@gmail.com>
2025-06-04 14:38:32 +0300

    formatted tf files

--- terraform/gcp/modules/load_balancer/variables.tf
+++ terraform/gcp/modules/load_balancer/variables.tf
@@ -1,7 +1,7 @@
 variable "project_id" {
   description = "GCP project ID."
   type        = string
-#  default       = "lofty-memento-458508-i1"
+  #  default       = "lofty-memento-458508-i1"
 }
 
 variable "load_balancer_name" {
