e494cb45c60b43929636fea66fe17512f798dcf6
iviul <iviul@outlook.com>
2025-06-11 18:51:06 +0300

    Metadata

--- terraform/gcp/metadata.sh
+++ terraform/gcp/metadata.sh
@@ -14,10 +14,3 @@ EOF
 chown ubuntu:ubuntu /home/ubuntu/.ssh/authorized_keys
 chmod 600 /home/ubuntu/.ssh/authorized_keys
 
-# Ensure Python3 is installed
-if ! command -v python3 >/dev/null 2>&1; then
-  apt-get update && apt-get install -y python3
-fi
-
-# Start a simple HTTP server on port 6443 for health checks
-nohup python3 -m http.server 6443 --bind 0.0.0.0 >/var/log/healthcheck.log 2>&1 &
