4e06043b91b39d577343b3606aec77bc0c2512b7
Oleh Kuzo <olegkuzo23@gmail.com>
2025-06-12 23:36:15 +0300

    Ansible Role for installing ops agent

--- terraform/gcp/metadata.sh
+++ terraform/gcp/metadata.sh
@@ -1,32 +1,14 @@
 #!/bin/bash
 
-exec > >(sudo tee /var/log/startup-script.log) 2>&1
-set -ex
-
-USER_HOME="/home/ubuntu"
-if [ ! -d "$USER_HOME" ]; then
-  USER_HOME="/home/$(whoami)"
-fi
-
 # Creating SSH directory
-mkdir -p $USER_HOME/.ssh
-chmod 700 $USER_HOME/.ssh
+mkdir -p /home/ubuntu/.ssh
+chmod 700 /home/ubuntu/.ssh
 
 # Adding SSH-keys into authorized_keys
-cat <<EOF > $USER_HOME/.ssh/authorized_keys
+cat <<EOF > /home/ubuntu/.ssh/authorized_keys
 ${ssh_keys}
 EOF
 
 # Rights
-chown $(basename $USER_HOME):$(basename $USER_HOME) $USER_HOME/.ssh/authorized_keys
-chmod 600 $USER_HOME/.ssh/authorized_keys
-
-# Update & install the Ops Agent
-sudo apt-get update -y || { echo "apt-get update failed"; exit 1; }
-sudo apt-get install -y curl gnupg || { echo "apt-get install failed"; exit 1; }
-curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh || { echo "curl failed"; exit 1; }
-sudo bash add-google-cloud-ops-agent-repo.sh --also-install || { echo "Ops Agent install failed"; exit 1; }
-
-# Start the Ops Agent
-sudo systemctl restart google-cloud-ops-agent || { echo "Ops Agent restart failed"; exit 1; }
-sudo systemctl status google-cloud-ops-agent --no-pager
+chown ubuntu:ubuntu /home/ubuntu/.ssh/authorized_keys
+chmod 600 /home/ubuntu/.ssh/authorized_keys
