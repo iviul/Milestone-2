bc00753d37f5e24d2d2f1a01dbbcf2105fb1c9c3
illusion4 <vladyslavlintur@gmail.com>
2025-06-19 11:13:15 +0300

    integrate static IP into ingress-nginx LoadBalancer setup on GKE

--- terraform/gcp/inventory.tf
+++ terraform/gcp/inventory.tf
@@ -1,17 +1,16 @@
 locals {
   db_name = "maindb" # Default database name
 
-   inventory = templatefile("${path.module}/../inventory.tpl", {
+  inventory = templatefile("${path.module}/../inventory.tpl", {
     private_ips      = module.vm.private_ips
     private_key_path = var.private_key_path
-    bastion_ip       = module.vm.public_ips["bastion"]
 
     db_host     = module.db-instance.db_hosts[local.db_name]
     db_user     = module.db-instance.db_users[local.db_name]
     db_password = module.db-instance.db_passwords[local.db_name]
     db_port     = module.db-instance.db_ports[local.db_name]
     db_name     = module.db-instance.db_names[local.db_name]
-    lb_ips = module.load-balancer.lb_name_to_ip_map
+    static_ips  = module.static_ips.ip_addresses
   })
 }
 
--- terraform/gcp/main.tf
+++ terraform/gcp/main.tf
@@ -58,16 +58,16 @@ module "monitoring" {
   cpu_usage_threshold        = local.config.monitoring.cpu_usage_threshold
 }
 
-module "load-balancer" {
-  source     = "./modules/load-balancer"
-  project_id = local.config.project.name
-  region     = local.region
-  zone       = "europe-west3-a"
-  network    = module.network.vpc_self_links[local.config.load_balancers[0].vpc]
-  instances  = module.vm.non_bastion_instances_self_links
+# module "load-balancer" {
+#   source     = "./modules/load-balancer"
+#   project_id = local.config.project.name
+#   region     = local.region
+#   zone       = "europe-west3-a"
+#   network    = module.network.vpc_self_links[local.config.load_balancers[0].vpc]
+#   instances  = module.vm.non_bastion_instances_self_links
 
-  load_balancers = local.config.load_balancers
-}
+#   load_balancers = local.config.load_balancers
+# }
 
 module "db-instance" {
   source            = "./modules/db-instance"
@@ -81,11 +81,16 @@ module "db-instance" {
   db_username       = local.db_username
 }
 
+module "static_ips" {
+  source      = "./modules/static_ips"
+  static_ips  = local.config.static_ips
+}
+
 module "cloudflare_dns" {
   source               = "../shared_modules/cloudflare_dns"
   cloudflare_zone_id   = var.cloudflare_zone_id
   dns_records_config   = local.config.dns_records
-  resource_dns_map     = module.load-balancer.lb_name_to_ip_map
+  resource_dns_map     = module.static_ips.ip_addresses
   cloudflare_api_token = var.cloudflare_api_token
 }
 
